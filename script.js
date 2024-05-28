45523c67d707621b8c65f1fbb7ca1c4fcde3f0a5
Gulzatko <Mamajanovna86@gmail.com>
2024-05-28 23:08:03 +0400

    fixed the fetching digital reales

--- script.js
+++ script.js
@@ -17,6 +17,8 @@ let currentPremiers = document
   .querySelector("#current_premiers")
   .addEventListener("click", (e) => {
     e.preventDefault();
+    const moviesEl = document.querySelector(".movies");
+    moviesEl.innerHTML= "";
     getMovies(API_URL_CURRENT_PREMIRS);
   });
 
@@ -24,19 +26,30 @@ let comimg = document
   .querySelector("#top_coming")
   .addEventListener("click", (e) => {
     e.preventDefault();
+    const moviesEl = document.querySelector(".movies");
+    moviesEl.innerHTML= "";
     getMovies(API_URL_COMING);
   });
   let bestMovies = document
   .querySelector("#top_best").addEventListener("click",(e) =>{
     e.preventDefault();
+    const moviesEl = document.querySelector(".movies");
+    moviesEl.innerHTML= "";
     getMovies(API_URL_TOP_BEST);
   });
+
   let digitalMovies = document
   .querySelector("#digital-releases")
   .addEventListener("click", (e) => {
     e.preventDefault();
-    getMovies(API_URL_DIGITAL);
+    const moviesEl = document.querySelector(".movies");
+    moviesEl.innerHTML= "";
+    getDigitalMovies(API_URL_DIGITAL);
   });
+
+  function searchApi(){
+    document.querySelector(".header_search").addEventListener
+  }
 
 async function getMovies(url) {
   const resp = await fetch(url, {
@@ -53,6 +66,44 @@ async function getMovies(url) {
 function showMovies(data) {
   const moviesEl = document.querySelector(".movies");
   data.items.forEach((movie) => {
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
+    movieEl.innerHTML = `
+            <div class="movie_cover-inner">
+              <img
+              src="${movie.posterUrlPreview}" 
+             class="movie_cover"
+             alt="${movie.nameRu}"
+             />
+              <div class="movie_cover--darkened"></div>
+             </div>
+             <div class="movie_info">
+               <div class="movie_title">${movie.nameRu}</div>
+               <div class="movie_category">${movie.genres.map(
+                 (genre) => `${genre.genre}`
+               )}
+               </div>
+             </div>
+            `;
+    moviesEl.appendChild(movieEl);
+  });
+ 
+}
+async function getDigitalMovies(url) {
+  const resp = await fetch(url, {
+    headers: {
+      "Content-Type": "application/json",
+      "X-API-KEY": API_KEY,
+    },
+  });
+  const respData = await resp.json();
+  console.log("maindata", respData);
+  showDigitMovies(respData);
+}
+
+function showDigitMovies(data) {
+  const moviesEl = document.querySelector(".movies");
+  data.releases.forEach((movie) => {
     const movieEl = document.createElement("div");
     movieEl.classList.add("movie");
     movieEl.innerHTML = `
