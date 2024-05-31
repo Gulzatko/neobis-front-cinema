7d6153be69b734511546f863be632642ceb7fef6
Gulzatko <Mamajanovna86@gmail.com>
2024-05-31 13:26:16 +0400

    resuced extra lines in js

--- script.js
+++ script.js
@@ -1,20 +1,7 @@
 const moviesEl = document.querySelector(".movies");
 const form = document.querySelector("form");
 const favoriteBtn = document.querySelector(".favourites_btn");
-
 const API_KEY = "9b07ab3a-b5b6-4699-9d16-f80f47e3af08";
-
-const API_URL_ALL =
-  "https://kinopoiskapiunofficial.tech/api/v2.2/films/collections?type=TOP_POPULAR_ALL&page=1&items=20";
-
-const API_URL_CURRENT_PREMIRS =
-  "https://kinopoiskapiunofficial.tech/api/v2.2/films/premieres?year=2024&month=MAY&page=1&items=20";
-
-const API_URL_COMING =
-  "https://kinopoiskapiunofficial.tech/api/v2.2/films?order=RATING&type=ALL&ratingFrom=0&ratingTo=10&yearFrom=1000&yearTo=3000&page=1&items=20";
-const API_URL_TOP_BEST =
-  "https://kinopoiskapiunofficial.tech/api/v2.2/films/collections?type=TOP_POPULAR_ALL&page=1&items=20";
-
 const API_URL_DIGITAL =
   "https://kinopoiskapiunofficial.tech/api/v2.1/films/releases?year=2024&month=MAY&page=1&releases=5";
 
@@ -32,7 +19,7 @@ function getRandomRating() {
 let moviesIdArray = JSON.parse(localStorage.getItem('id')) ? JSON.parse(localStorage.getItem('id')) : []
 
 //fetching data for main page
-getMovies(API_URL_ALL);
+getMovies(`https://kinopoiskapiunofficial.tech/api/v2.2/films/collections?type=TOP_POPULAR_ALL&page=1&items=20`);
 
 // fetching data for input value
 form.addEventListener("submit", (e) => {
@@ -88,12 +75,14 @@ function showInputMovies(data) {
   });
 }
 
+// addEvenListeners for current premiers, coming, best moviesEl, digital movies 
+
 let currentPremiers = document
   .querySelector("#current_premiers")
   .addEventListener("click", (e) => {
     e.preventDefault();
     moviesEl.innerHTML = "";
-    getMovies(API_URL_CURRENT_PREMIRS);
+    getMovies(`https://kinopoiskapiunofficial.tech/api/v2.2/films/premieres?year=2024&month=MAY&page=1&items=20`);
   });
 
 let comimg = document
@@ -101,14 +90,14 @@ let comimg = document
   .addEventListener("click", (e) => {
     e.preventDefault();
     moviesEl.innerHTML = "";
-    getMovies(API_URL_COMING);
+    getMovies(`https://kinopoiskapiunofficial.tech/api/v2.2/films?order=RATING&type=ALL&ratingFrom=0&ratingTo=10&yearFrom=1000&yearTo=3000&page=1&items=20`);
   });
 let bestMovies = document
   .querySelector("#top_best")
   .addEventListener("click", (e) => {
     e.preventDefault();
     moviesEl.innerHTML = "";
-    getMovies(API_URL_TOP_BEST);
+    getMovies(`https://kinopoiskapiunofficial.tech/api/v2.2/films/collections?type=TOP_POPULAR_ALL&page=1&items=20`);
   });
 
 let digitalMovies = document
@@ -116,7 +105,7 @@ let digitalMovies = document
   .addEventListener("click", (e) => {
     e.preventDefault();
     moviesEl.innerHTML = "";
-    getDigitalMovies(API_URL_DIGITAL);
+    getDigitalMovies(`https://kinopoiskapiunofficial.tech/api/v2.1/films/releases?year=2024&month=MAY&page=1&releases=5`);
   });
 
 async function getMovies(url) {
@@ -132,8 +121,7 @@ async function getMovies(url) {
 }
 
 function showMovies(data) {
-
-  data.items.forEach((movie) => {
+     data.items.forEach((movie) => {
     const movieEl = document.createElement("div");
     movieEl.classList.add("movie");
     movieEl.innerHTML = `
@@ -174,6 +162,7 @@ async function getDigitalMovies(url) {
   showDigitMovies(respData);
 }
 
+// seperate function for showDigitMovies
 function showDigitMovies(data) {
   data.releases.forEach((movie) => {
     const movieEl = document.createElement("div");
@@ -186,25 +175,26 @@ function showDigitMovies(data) {
              alt="${movie.nameRu}"
              />
               <div class="movie_cover--darkened"></div>
-             </div>
+            
              <div class="movie_info">
-              <div class="info_top">
+             <div class="info-top">
+             <h3 class="movie-rating"></h3> 
+              <div  class="heart" id="heart"></div> 
+              </div>
+              <div class="info_bottom">
                <div class="movie_title">${movie.nameRu}</div>
                <div class="movie_category">${movie.genres.map(
-      (genre) => `${genre.genre}`
-    )} </div>
+              (genre) => `${genre.genre}`
+                )} </div>
                  
-                     <div class="info-top">
-                        <h3 class="movie-rating"></h3> 
-                         <div  class="heart" id="heart"></div> 
-                    </div>
-                
-              </div>  
-             </div>
+                     </div>  
+               </div>
+               </div>
             `;
     moviesEl.appendChild(movieEl);
   });
 }
+
 async function selectFavorite(url) {
   moviesEl.innerHTML = '';
   try {
